7e874cd467608c9042fb7812263652444a0a789d
Kate Cevora <kate.wilkinson@illumr.com>
2023-10-19 10:43:00 +0100

    Updated to 502

--- run_nnUNet.sh
+++ run_nnUNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=18:mem=100gb:ngpus=1:gpu_type=RTX6000
-#PBS -N nnUNet_KITS_501
+#PBS -N nnUNet_KITS_502
 
 cd ${PBS_O_WORKDIR}
 
@@ -14,8 +14,8 @@ python -c "import torch;print(torch.cuda.is_available())"
 
 # Set environment variables
 ROOT_DIR='/rds/general/user/kc2322/projects/cevora_phd/live/kits19/'
-DATASET='Dataset501_Fold0'
-TASK=501
+DATASET='Dataset502_Fold0'
+TASK=502
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
