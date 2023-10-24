1e63b5a3c599366b15fa265784d323a707ca59bc
Kate Cevora <kate.wilkinson@illumr.com>
2023-10-24 14:23:39 +0100

    Updated to 700

--- predict_nnUNet.sh
+++ predict_nnUNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=15:00:00
 #PBS -l select=1:ncpus=15:mem=120gb:ngpus=1:gpu_type=RTX6000
-#PBS -N nnUNet_KITS_predict_600
+#PBS -N nnUNet_KITS_predict_700
 
 cd ${PBS_O_WORKDIR}
 
@@ -15,7 +15,7 @@ python -c "import torch;print(torch.cuda.is_available())"
 # Set environment variables
 ROOT_DIR='/rds/general/user/kc2322/projects/cevora_phd/live/kits19/'
 
-datasets=("Dataset600_Fold1" "Dataset600_Fold1" "Dataset600_Fold1")
+datasets=("Dataset700_Fold2" "Dataset700_Fold2" "Dataset700_Fold2")
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
