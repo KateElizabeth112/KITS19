4a60129f16fda522e339be0883931721ccf86039
Kate Cevora <kate.wilkinson@illumr.com>
2023-10-25 10:10:14 +0100

    Updated to 700

--- predict_nnUNet.sh
+++ predict_nnUNet.sh
@@ -15,7 +15,7 @@ python -c "import torch;print(torch.cuda.is_available())"
 # Set environment variables
 ROOT_DIR='/rds/general/user/kc2322/projects/cevora_phd/live/kits19/'
 
-datasets=("Dataset700_Fold2" "Dataset700_Fold2" "Dataset700_Fold2")
+datasets=("Dataset700_Fold2" "Dataset701_Fold2" "Dataset702_Fold2")
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
